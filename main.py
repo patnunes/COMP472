9e9732085167002cc9afd479b19fb8968ea42a80
Louis-Benjamin <lbduro9012@gmail.com>
2020-11-18 18:39:45 -0500

    Auto stash before merge of "UCS" and "origin/UCS"

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 from uniform_cost import uniform_cost
 from puzzle import Puzzle
-from write_functions import write_solution, write_search_path
+from write_functions import write_solution, write_search
 
 
 def main():
@@ -9,16 +9,16 @@ def main():
     puzzle3 = Puzzle([1, 0, 3, 6, 5, 2, 7, 4], 4, 2)
 
     start = time.time()
-    goal_node, total_cost = uniform_cost(puzzle3)
+    goal_node, total_cost, closedlist = uniform_cost(puzzle3)
+    
     end = time.time()
     execution_time = end - start
     print(execution_time)
-    # #input_file = './output'
+
     output_directory = './output'
 
-    # # uniform_cost(puzzle3)
     write_solution(output_directory, execution_time, goal_node, total_cost, 'ucs')
-    # # write_search_path(output_directory, execution_time, solution_path, 'ucs')
+    write_search(output_directory, closedlist, 'ucs')
 
 
 main()
