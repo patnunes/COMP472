9f2f206804d01e34c554566a95e4800e2de84da6
Louis-Benjamin <lbduro9012@gmail.com>
2020-11-18 04:22:49 -0500

    removed node

--- node.py
+++ node.py
@@ -1,10 +1,11 @@
-
-import time
+import random
 from puzzle import Puzzle
+
 class Node:
     """A node. Contains pointer to the parent"""
 
-    def __init__(self, state, parent=None, action=None, g_fxn=0):
+    def __init__(self, state, parent=None, action=None, heuristic=''):
+        self.id = random.randrange(2147483647)
         self.state = state
         self.parent = parent
         self.g_fxn = 0  # Distance to start node, cost function
@@ -15,29 +16,52 @@ class Node:
         if parent:
             self.depth = parent.depth + 1
 
+    def __hash__(self):
+        return hash(self.id)
+
+    def __eq__(self, other):
+        return isinstance(other, Node) and self.state == other.state
+
+    def set_g(self, new_g):
+        self.g_fxn = new_g
+
+    def set_h(self):
+        temp = 0
+        for i in range(0, self.state.puzzle_size):
+            if self.state.goal_state_1[i] != self.state.puzzle[i]:
+                temp += 1
+        self.h_fxn = temp
+
+    def set_f(self):
+        self.f_fxn = self.g_fxn + self.h_fxn
+
     def __repr__(self):
-        return '{0}(action:{1}, g(node)={2}, h(node)={3}, f(node)={4})'.format(self.state, self.action, self.g_fxn, self.h_fxn, self.f_fxn)
-    
-    def less_h_fxn(self, other):
-        """compares h_fxn value for 2 nodes"""
-        return self.h_fxn < other.h_fxn
-    
-    def less_g_fxn(self, other):
-        """compares g_fxn value for 2 nodes"""
-        return self.g_fxn < other.g_fxn
-    
-    def __lt__(self, other):
-        """For use with A*, compares f_fxn between two nodes"""
-        return self.f_fxn < other.f_fxn
+        return '{0}(action:{1}, g(node)={2}, h(node)={3}, f(node)={4}, depth={5}, id={6})'.format(
+            self.state, self.action, self.g_fxn, self.h_fxn, self.f_fxn, self.depth, self.id
+        )
 
-    def expand(self, puzzle):
+    def expand(self):
         """List all states reachable in one step from current state."""
-        return [self.child_node(puzzle, action)
-                for action in puzzle.actions(self.state)]
+        children = set()
+        action_space = self.state.actions()
+        print("LISTACT", action_space)
+        for action in action_space:
+            children.add(self.child_node(action))
 
-    def child_node(self, puzzle, action):
-        next_state = puzzle.result(self.state, action)
-        next_node = Node(next_state, self, action, puzzle.g_fxn(self.g_fxn, self.state, action, next_state))
+        if(len(action_space) != len(children)):
+            print('disaster')
+
+        return children
+
+    def child_node(self, action):
+        """Creates the new state based on the action given"""
+        next_state = self.state.result(action)
+        print('next_state')
+        print(next_state)
+        next_node = Node(next_state, self, action)
+        next_node.set_g(self.g_fxn+next_state.get_cost())
+        next_node.set_f()
+        next_node.set_h()
         return next_node
 
     def solution_path(self):
@@ -47,38 +71,3 @@ class Node:
             path_back.append(node)
             node = node.parent
         return list(reversed(path_back))
-
-    def __eq__(self, other):
-        return isinstance(other, Node) and self.state == other.state
-
-def uniform_cost(puzzle):
-    start = time.time()
-    node = Node(puzzle())
-    openlist = []
-    openlist.append(node)
-    closedlist = []
-
-    while openlist:
-        node = openlist.pop()
-        if puzzle.goal_test(node.state):
-            print(len(closedlist), "paths have been expanded and", len(openlist), "paths remain in the openlist")
-            return
-        closedlist.append(node.state)
-        for child in node.expand(puzzle):
-            print(child)
-            if child.state not in closedlist and child not in openlist:
-                 openlist.append(child)
-            elif child in openlist:
-                if node.less_g_fxn(child):
-                    del openlist[child]
-                    openlist.append(child)
-                    openlist.sort(node.g_fxn)
-            
-        now = time.time()
-        if (now - start) > 600:
-            return
-
-
-puzzle1 = Puzzle([3, 0, 1, 4, 2, 6, 5, 7], 2,4)
-
-uniform_cost(puzzle1)
