8e4c6bc69df86ed681dc267b8d6902dd8b7ad299
Louis-Benjamin <lbduro9012@gmail.com>
2020-11-18 23:04:58 -0500

    temp analysis files

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ def main():
             puzzle = Puzzle(temp_puzzle, cols, rows)
 
             
-            # goal_node, total_cost, closedlist, execution_time = uniform_cost(puzzle)
-            # write_solution(puzzle_ctr ,output_directory, execution_time, goal_node, total_cost, 'ucs')
-            # write_search(puzzle_ctr , output_directory,execution_time, closedlist, 'ucs')
+            goal_node, total_cost, closedlist, execution_time = uniform_cost(puzzle)
+            write_solution(puzzle_ctr ,output_directory, execution_time, goal_node, total_cost, 'ucs')
+            write_search(puzzle_ctr , output_directory,execution_time, closedlist, 'ucs')
 
             goal_node, total_cost, closedlist, execution_time = best_first_search(puzzle, heuristic='h1')
             # write_solution(puzzle_ctr ,output_directory, execution_time, goal_node, total_cost, 'gbfs', 'h0' )
@@ -36,14 +36,14 @@ def main():
             write_solution(puzzle_ctr ,output_directory, execution_time, goal_node, total_cost, 'gbfs', 'h2' )
             write_search(puzzle_ctr , output_directory,execution_time, closedlist, 'gbfs', 'h2')
 
-            # goal_node, total_cost, closedlist, execution_time = a_star(puzzle, heuristic='h1')
-            # # write_solution(puzzle_ctr ,output_directory, execution_time, goal_node, total_cost, 'astar', 'h0' )
-            # # write_search(puzzle_ctr , output_directory,execution_time, closedlist, 'astar', 'h0')
-            # write_solution(puzzle_ctr ,output_directory, execution_time, goal_node, total_cost, 'astar', 'h1' )
-            # write_search(puzzle_ctr , output_directory,execution_time, closedlist, 'astar', 'h1')
-            # goal_node, total_cost, closedlist, execution_time = a_star(puzzle, heuristic='h2')
-            # write_solution(puzzle_ctr ,output_directory, execution_time, goal_node, total_cost, 'astar', 'h2' )
-            # write_search(puzzle_ctr , output_directory,execution_time, closedlist, 'astar', 'h2')
+            goal_node, total_cost, closedlist, execution_time = a_star(puzzle, heuristic='h1')
+            # write_solution(puzzle_ctr ,output_directory, execution_time, goal_node, total_cost, 'astar', 'h0' )
+            # write_search(puzzle_ctr , output_directory,execution_time, closedlist, 'astar', 'h0')
+            write_solution(puzzle_ctr ,output_directory, execution_time, goal_node, total_cost, 'astar', 'h1' )
+            write_search(puzzle_ctr , output_directory,execution_time, closedlist, 'astar', 'h1')
+            goal_node, total_cost, closedlist, execution_time = a_star(puzzle, heuristic='h2')
+            write_solution(puzzle_ctr ,output_directory, execution_time, goal_node, total_cost, 'astar', 'h2' )
+            write_search(puzzle_ctr , output_directory,execution_time, closedlist, 'astar', 'h2')
     file.close()
 
 main()
