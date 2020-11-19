0f40e9a3cfe3be5ed7e4a0d333068c01a16085dc
Alan Giamatti <alan.giamatti@gmail.com>
2020-11-19 01:40:52 -0500

    minor changes

--- write_functions.py
+++ write_functions.py
@@ -4,47 +4,47 @@ from greedys_best_first import best_first_search
 from a_star import a_star
 from puzzle import Puzzle
 
-def write_solution(ctr,output_directory, execution_time, goal_node, total_cost, algo, heuristic=None):
-        if heuristic is None:
-            file_name = '%s_%s_solution.txt' % (ctr, algo)
-        else:
-            file_name = '%s_%s-%s_solution.txt' % (ctr, algo, heuristic)
 
-
-        if execution_time >= 60:
-            with open(output_directory + '/' + file_name, 'w') as file:
-                file.write('No solution')
-        else:
-            solution_path = goal_node.solution_path()
-            output_lines = []
-            for node in solution_path:
-                
-                output_lines.append((str(node.moved_index)+' '+str(node.single_action_cost)+' '+str(node.state) ).replace('[', '').replace(']', '').replace(',', '').replace('\n',' ').replace(',','\n').strip())
-            output_lines.append('%d %f' % (total_cost, execution_time))
-            with open(output_directory + '/' + file_name, 'w') as file:
-                file.writelines("%s\n" % i for i in output_lines)
-        file.close()
-
-
-
-def write_search(ctr,output_directory, execution_time,  closed_list,  algo, heuristic=None):
-    print(closed_list)
+def write_solution(ctr, output_directory, execution_time, goal_node, total_cost, algo, heuristic=None):
     if heuristic is None:
-            file_name = '%s_%s_search.txt' % (ctr, algo)
+        file_name = '%s_%s_solution.txt' % (ctr, algo)
     else:
-            file_name = '%s_%s-%s_search.txt' % (ctr, algo, heuristic)
-    
+        file_name = '%s_%s-%s_solution.txt' % (ctr, algo, heuristic)
+
+    if execution_time >= 60:
+        with open(output_directory + '/' + file_name, 'w') as file:
+            file.write('No solution')
+    else:
+        solution_path = goal_node.solution_path()
+        output_lines = []
+        for node in solution_path:
+
+            output_lines.append((str(node.moved_index)+' '+str(node.single_action_cost)+' '+str(node.state)).replace(
+                '[', '').replace(']', '').replace(',', '').replace('\n', ' ').replace(',', '\n').strip())
+        output_lines.append('%d %f' % (total_cost, execution_time))
+        with open(output_directory + '/' + file_name, 'w') as file:
+            file.writelines("%s\n" % i for i in output_lines)
+    file.close()
+
+
+def write_search(ctr, output_directory, execution_time, closed_list, algo, heuristic=None):
+    if heuristic is None:
+        file_name = '%s_%s_search.txt' % (ctr, algo)
+    else:
+        file_name = '%s_%s-%s_search.txt' % (ctr, algo, heuristic)
+
     if execution_time > 60:
-            with open(output_directory + '/' + file_name, 'w') as file:
-                file.write('No solution')
+        with open(output_directory + '/' + file_name, 'w') as file:
+            file.write('No solution')
 
     else:
         file = open(output_directory + '/' + file_name, 'w')
         with open(output_directory + '/' + file_name, 'w') as file:
-            file.writelines("%s\n" % i for i in closed_list) 
+            file.writelines("%s\n" % i for i in closed_list)
     file.close()
 
-def write_analysis(output_directory, fifty_puzzle_file, algo, cols, rows, puzzle_qty, heuristic=None, restrict_time =True, ctr = None ):
+
+def write_analysis(output_directory, fifty_puzzle_file, algo, cols, rows, puzzle_qty, heuristic=None, restrict_time=True, ctr=None):
 
     if heuristic is None:
         file_name = '%s_analysis.txt' % (algo)
@@ -53,23 +53,25 @@ def write_analysis(output_directory, fifty_puzzle_file, algo, cols, rows, puzzle
     else:
         file_name = '%s-%s_analysis.txt' % (algo, heuristic)
 
-    total_length_solution =0
+    total_length_solution = 0
     total_length_search = 0
     total_nb_nosolution = 0
     total_cost_50 = 0
     total_execution_time = 0
-    
+
     with open(fifty_puzzle_file) as file:
         for line in file:
-            temp_puzzle = list(map(int ,line.split(' ')))
+            temp_puzzle = list(map(int, line.split(' ')))
             puzzle = Puzzle(temp_puzzle, cols, rows)
 
             if algo == 'ucs':
                 goal_node, total_cost, closedlist, execution_time = uniform_cost(puzzle)
             elif algo == 'gbfs':
-                goal_node, total_cost, closedlist, execution_time = best_first_search(puzzle, heuristic= heuristic, restrict_time=restrict_time)
+                goal_node, total_cost, closedlist, execution_time = best_first_search(
+                    puzzle, heuristic=heuristic, restrict_time=restrict_time)
             elif algo == 'astar':
-                goal_node, total_cost, closedlist, execution_time = a_star(puzzle, heuristic=heuristic)
+                goal_node, total_cost, closedlist, execution_time = a_star(
+                    puzzle, heuristic=heuristic)
 
             total_length_solution += len(goal_node.solution_path())
             total_length_search += len(closedlist)
