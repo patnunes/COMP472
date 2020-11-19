ded8bd83659d2b43809aaa567c7c884b8b78f9fa
Louis-Benjamin <lbduro9012@gmail.com>
2020-11-18 19:03:03 -0500

    removed ucs + changed ucs algo

--- uniform_cost.py
+++ uniform_cost.py
@@ -21,13 +21,34 @@ def uniform_cost(puzzle):
             print("Solution achieved")
             print(len(closed_list), "paths have been expanded and",
                   len(open_list), "paths remain in the open_list")
-            return node, node.g_fxn
+            return node, node.g_fxn, closed_list
         closed_list.append(node)
+
         for child in node.expand():
-            visited_nodes += 1
-            if child not in closed_list and child not in open_list:
-                open_list.append(child)
-            elif child in open_list:
-                if child.g_fxn < node.g_fxn:
-                    open_list.remove(child)
-                    open_list.append(child)
+            continue_loop = False
+            for closed_node in closed_list:
+                if child.state == closed_node.state:
+                    continue_loop = True
+                    break
+
+            if continue_loop:
+                continue
+
+            for open_node in open_list:
+                if open_node.state == child.state and child.g_fxn > open_node.g_fxn:
+                    continue_loop = True
+                    break
+
+            if continue_loop:
+                continue
+
+            open_list.append(child)
+
+        # for child in node.expand():
+        #     visited_nodes += 1
+        #     if child not in closed_list and child not in open_list:
+        #         open_list.append(child)
+        #     elif child in open_list:
+        #         if child.g_fxn < node.g_fxn:
+        #             open_list.remove(child)
+        #             open_list.append(child)
