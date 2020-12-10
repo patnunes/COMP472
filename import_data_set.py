1838372e8e42f5cab73ed2ac8f11ae5a660c3bcc
Alan Giamatti <alan.giamatti@gmail.com>
2020-12-09 23:56:17 -0500

    just some documentation

--- import_data_set.py
+++ import_data_set.py
@@ -1,8 +1,10 @@
-import numpy as np
-from pandas import read_csv
+""" imports data set and places it in DataSet class
+"""
 
 
 def import_data_set():
+    """ imports data set
+    """
     training_file = open("./a3-dataset/covid_training.tsv", "r", encoding="utf-8")
     testing_file = open("./a3-dataset/covid_test_public.tsv", "r", encoding="utf-8")
 
@@ -24,11 +26,17 @@ def import_data_set():
         testing_inputs.append_set(Tweet(col[0], col[1], col[2]))
 
     print(training_inputs)
-    print(training_inputs.get_most_used_words())
-    # print(testing_inputs)
+    most_used_words = training_inputs.get_most_used_words()
+    for elem in most_used_words:
+        print(elem)
+
+    return training_inputs, testing_inputs
 
 
 class Tweet:
+    """ The class holding a single tweet with its ID, text and label
+    """
+
     def __init__(self, tweet_id, text, label):
         self.tweet_id = tweet_id
         self.text = text
@@ -39,6 +47,9 @@ class Tweet:
 
 
 class TweetWord:
+    """ The class containing unique word statistics
+    """
+
     def __init__(self, tweet_origin, string):
         self.string = string
         self.usage = 0
@@ -48,6 +59,8 @@ class TweetWord:
         self.set_label(tweet_origin)
 
     def set_label(self, tweet_origin):
+        """ If the TweetWord is not unique, we want to use set_label to note more usage
+        """
         self.mentioned_tweets.append(tweet_origin)
         self.usage += 1
         if tweet_origin.label == 'yes':
@@ -56,7 +69,8 @@ class TweetWord:
             self.label_no = self.label_no + 1
 
     def __repr__(self):
-        return f'Word: {self.string}, y/n: {self.label_yes}/{self.label_no}, Count: {self.usage}, Mentioned in: {len(self.mentioned_tweets)}\n'
+        return (f'Word: {self.string}, y/n: {self.label_yes}/{self.label_no}, '
+                f'Count: {self.usage}, Mentioned in: {len(self.mentioned_tweets)}')
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
@@ -68,10 +82,15 @@ class TweetWord:
         return hash(self.string)
 
     def asdict(self):
-        return {'string': self.string, 'usage': self.usage, 'yes': self.label_yes, 'no': self.label_no}
+        """ returns a dict of the object Tweet
+        """
+        return {'string': self.string, 'yes': self.label_yes, 'no': self.label_no}
 
 
 class DataSet:
+    """ The full data set object holding the tweets and the individual words
+    """
+
     def __init__(self):
         self.full_input = []
         self.total_words = 0
@@ -81,6 +100,8 @@ class DataSet:
         self.yes_label = []
 
     def append_set(self, input_tweet):
+        """ Populate the data set by appending Tweet Objects to it
+        """
         self.full_input.append(input_tweet)
         input_words = input_tweet.text.split(' ')
         for word in input_words:
@@ -99,15 +120,23 @@ class DataSet:
                 self.unique_words_actual.add(tweet_word.string)
 
     def get_unique_words_count(self):
+        """ returns total numbers of unique words (ie vocabulary)
+        """
         return len(self.unique_words)
 
     def get_total_words(self):
+        """ returns total words
+        """
         return len(self.total_words)
 
     def get_unique_words(self):
+        """ returns unique words set (for some reason?)
+        """
         return self.unique_words
 
     def get_most_used_words(self):
+        """ gets the 20 most used words
+        """
         word_list = []
         for known_word in self.unique_words:
             word_list.append(known_word)
@@ -115,7 +144,8 @@ class DataSet:
         return word_list[:20]
 
     def __repr__(self):
-        return f'Unique words: {len(self.unique_words)} ({len(self.unique_words_actual)}), Total words: {self.total_words}, Total entries: {len(self.full_input)}'
+        return (f'Unique words: {len(self.unique_words)}({len(self.unique_words_actual)}),'
+                f' Total words: {self.total_words}, Total entries: {len(self.full_input)}')
 
 
 def transform(word):
