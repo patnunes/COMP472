f24306e12be16066a3979fd44cd948e0cda0a2b0
Louis-Benjamin <lbduro9012@gmail.com>
2020-12-11 15:45:34 -0500

    removed some files

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import csv
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.tokenize import RegexpTokenizer
 import naive_bayes 
 import vocabulary
+from import_data_set import DataSet, Tweet, TweetWord
 
 def main():
 
-    tsv_file = './a3-dataset/covid_training.tsv'
-    
-    OV_vocab = vocabulary.Vocabulary(tsv_file)
-    print (OV_vocab.get_vocabulary_size())
-    #used to calculate probability of classifier, i.e. c1 = yes, c2 = no
-    naive_bayes.prob_class(OV_vocab.data, classifier_word='no')
-    
-    #total # of times the word appears in a negative case
-    naive_bayes.prob_word_given_class(OV_vocab.matrix, word='zombie',classifier_word='no')
-    
-    OV_vocab.get_total_words_class('no')
+    training_file = open("./a3-dataset/covid_training.tsv", "r", encoding="utf-8")
+    testing_file = open("./a3-dataset/covid_test_public.tsv", "r", encoding="utf-8")
+
+    next(training_file)  # skip first line
+    next(testing_file)  # skip first line
+
+    training_lines = training_file.readlines()
+    testing_lines = testing_file.readlines()
+
+    training_inputs = DataSet()
+    testing_inputs = DataSet()
+
+    for line in training_lines:
+        col = line.split('\t')
+        training_inputs.append_set(Tweet(col[0], col[1], col[2]))
+
+    for line in testing_lines:
+        col = line.split('\t')
+        testing_inputs.append_set(Tweet(col[0], col[1], col[2]))
+
+    print(training_inputs)
+    print(testing_inputs)
+
+    most_used_words = training_inputs.get_most_used_words()
+    for elem in most_used_words:
+        print(elem)
+
+    training_inputs.implement_filtered_vocabulary()
+    print(training_inputs)
+    return training_inputs, testing_inputs
 
 main()
